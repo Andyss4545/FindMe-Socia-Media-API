e375490be20c72361a5e2f18e24bf446274bddb6
GentleAndy <andystemidwebguru@gmail.com>
2023-03-22 04:39:26 -0700

    Added follow and unfollow user routes

--- mysocial-backend/routes/users.js
+++ mysocial-backend/routes/users.js
@@ -1,19 +1,19 @@
 const route = require('express')
-const { getUsers, getUser, postUser, updateUser, deleteUser } = require('../controllers/userController')
+const { getUser, updateUser, deleteUser, followUser, unfollowUser } = require('../controllers/userController')
 const router = route.Router()
 
 
+// Note: We don't get all users in social media application
 
 // Get a user
 router.get('/:id', getUser)
-// create a user
-router.post('/', postUser)
 // update a user
 router.put('/:id', updateUser)
 // delete a user
 router.delete('/:id', deleteUser)
 // follow a user
-
+router.put('/:id/follow', followUser)
 // unfollow a user
+router.put('/:id/unfollow', unfollowUser)
 
 module.exports = router
