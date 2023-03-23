a6be206c57eca6c23e61cafca0794008fb2e2d0c
GentleAndy <andystemidwebguru@gmail.com>
2023-03-23 03:29:42 -0700

    Added a model for our posts

---
+++ mysocial-backend/models/postModel.js
@@ -0,0 +1,29 @@
+const mongoose  = require('mongoose')
+
+
+const postSchema = new mongoose.Schema({
+        
+         userId: {
+              type: String,
+              required: true,
+         },
+
+         desc: {
+             type: String,
+             max: 500
+         },
+
+         img: {
+              type: String
+         },
+
+         likes: {
+             default: []
+         }
+},
+  {timestamps: true}
+)
+
+
+
+module.exports = mongoose.model('Post', postSchema)
