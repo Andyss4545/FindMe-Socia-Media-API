0ead602a5b0ae866a2d825cc1432bc6c476212de
GentleAndy <andystemidwebguru@gmail.com>
2023-03-24 05:41:24 -0700

    updated the route for getting posts timeline

--- mysocial-backend/routes/posts.js
+++ mysocial-backend/routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {createPost, updatePost, deletePost, likePost, getPost, getPosts}  = require('../controllers/postController')
+const {createPost, updatePost, deletePost, getPost, getPosts, likePost}  = require('../controllers/postController')
 
 
 // create a post route
@@ -8,13 +8,13 @@ router.post('/', createPost)
 // update a post route
 router.put('/:id', updatePost)
 // delete a post route
-router.delete('/', deletePost)
+router.delete('/:id', deletePost)
 // like a post route
-router.put('/', likePost)
+router.put('/:id/like', likePost)
 // get a post route
-router.get('/', getPost)
+router.get('/:id', getPost)
 // get timeline post route
-router.get('/', getPosts)
+router.get('/timeline/all', getPosts)
 
 
 module.exports = router
